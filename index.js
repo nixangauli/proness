a9986d2aac5c2421489ce355bcd28be42e5d7c4f
nixangauli <nixangauli5@gmail.com>
2022-07-24 21:28:01 +0545

    Bot Starting

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const { loadEvents } = require("./Handlers/eventHandler");
+
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+
+client.config = require("./config.json");
+
+client
+  .login(client.config.token)
+  .then(() => {
+    loadEvents(client);
+  })
+  .catch((err) => console.log(err));
